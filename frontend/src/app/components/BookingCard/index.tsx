b84557c260cb5ab9e8b3aacb9afb00013eb0c4fd
RafaelNMoura <rafaelnm.dev@gmail.com>
2023-04-11 20:40:04 -0300

    Alterações para o funcionamento dos testes do BookingCard e da SearchBar

---
+++ frontend/src/app/components/BookingCard/index.tsx
@@ -0,0 +1,114 @@
+import { IconMinus, IconPlus } from '../../assets/icons';
+import { ClickableIcon, Row, SizedBoxHorizontal, SizedBoxVertical } from '../../utils/utils';
+import styled from "styled-components";
+import { Input } from '../Input';
+
+const MakeReservationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-width: 450px;
+  align-items: left;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  padding-left: 30px;
+  padding-right: 30px;
+`;
+
+const DescriptionReservationMakeText = styled.span`
+  color: #717171;
+  font-size: 20px;
+  align-self: flex-start;
+`;
+
+const RadioLabel = styled.p`
+  margin-top: -5px;
+  align-self: flex-start;
+`;
+
+interface BookingCardProps {
+    guestNumber: number;
+    radioHandler: (event: React.ChangeEvent<HTMLInputElement>) => void;
+    setMinusIcon: () => void;
+    setPlusIcon: () => void;
+}
+
+function BookingCard({ guestNumber, radioHandler, setPlusIcon, setMinusIcon }: BookingCardProps) {
+    return (
+        <fieldset>
+            <MakeReservationContainer>
+                <Row>
+                    <DescriptionReservationMakeText>
+                        Hóspedes
+                    </DescriptionReservationMakeText>
+                    <ClickableIcon
+                        onClick={setPlusIcon} data-testid="plus-icon"
+                    >
+                        {IconPlus}
+                    </ClickableIcon>
+                    <DescriptionReservationMakeText>
+                        {guestNumber}
+                    </DescriptionReservationMakeText>
+                    <ClickableIcon
+                        onClick={setMinusIcon} data-testid="minus-icon"
+                    >
+                        {IconMinus}
+                    </ClickableIcon>
+                </Row>
+                <SizedBoxVertical />
+                <DescriptionReservationMakeText>
+                    Forma de Pagamento
+                </DescriptionReservationMakeText>
+                <SizedBoxVertical />
+                <label htmlFor="pix">
+                    <RadioLabel>
+                        <input
+                            type="radio"
+                            name="payment"
+                            value="Pix"
+                            id="pix"
+                            onChange={radioHandler}
+                        />
+                        <DescriptionReservationMakeText>
+                            Pix
+                        </DescriptionReservationMakeText>
+                    </RadioLabel>
+                </label>
+
+                <RadioLabel>
+                    <input
+                        type="radio"
+                        name="payment"
+                        value="CartaoDeCredito"
+                        id="cc"
+                        onChange={radioHandler}
+                    />
+                    <DescriptionReservationMakeText>
+                        Cartão de Crédito
+                    </DescriptionReservationMakeText>
+                </RadioLabel>
+
+                <RadioLabel>
+                    <input
+                        type="radio"
+                        name="payment"
+                        value="CartaoDeDebito"
+                        id="cd"
+                        onChange={radioHandler}
+                    />
+                    <DescriptionReservationMakeText>
+                        Cartão de Débito
+                    </DescriptionReservationMakeText>
+                </RadioLabel>
+                <Row>
+                    <Input size="MEDIUM" placeholder="Número do cartão"></Input>
+                    <SizedBoxHorizontal />
+                    <Input size="SMALL" placeholder="CVV"></Input>
+                </Row>
+                <SizedBoxVertical />
+                <Input size="MEDIUM" placeholder="Validade"></Input>
+            </MakeReservationContainer>
+        </fieldset>
+    );
+}
+
+export default BookingCard;
